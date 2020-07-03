a8b18d6c33e6dbd1640fc8dec4e66f52175d9cb7
Himanshi Jain <jainhimanshi97@gmail.com>
2020-07-03 17:21:15 +0530

    Sorting

--- Sorting and Searching/Insertionsort.java
+++ Sorting and Searching/Insertionsort.java
@@ -7,16 +7,20 @@ public class Insertionsort {
 	public static void main(String[] args) {
 		int [] arr= {5,4,3,6,1};
 		int n=arr.length;
-		arr=insertionsort(arr,n);
-		System.out.print(Arrays.toString(arr));
+		insertionsort(arr,0,arr.length-1);
 	}
-   public static void insertionsort(int[] arr,int n) {
-	   if(n==1)
-		   return ;
-	   else
-	   {
-		   
-	   }
+   public static void insertionsort(int[] arr,int start,int end) {
 	   
+	   for(int i=start+1;i<=end;i++) {
+		   int key=arr[i];
+		   int j=i-1;
+		   
+		   while(j>=0&&arr[j]>key) {
+			   arr[j+1]=arr[j];
+			   j--;
+		   }
+		   arr[j+1]=key;
+	   }
+	   System.out.print(Arrays.toString(arr));
    }
 }
